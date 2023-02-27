c5ad2314815e2cbf509a73ebc8d9547507cf11ba
sherwinromano <sherwinromano77@gmail.com>
2023-02-27 15:55:17 +0800

    Updated commit

--- script.js
+++ script.js
@@ -2,25 +2,25 @@ document.querySelector("#order").addEventListener("click", () => {
     alert("This page is not available yet");
 });
 
-
-
 document.querySelector("#submit-feedback").addEventListener("click", () => {
     let feedback = document.querySelector("#feedback");
     let card = document.querySelector(".card-result");
-    let feed = document.querySelector("#result");
+    let newElement = document.createElement("p");
     let overlay = document.querySelector("#overlay");
+    let result = document.querySelector(".result-section");
     
     if(feedback.value === "") {
         alert("Please input some feedback");
     } else {
         card.classList.add("card-opened");
         overlay.classList.add("active");
-        feed.append(feedback.value);
+        newElement.append(feedback.value);
+        result.append(newElement);
         // Close pop-up function
         document.querySelector("#close").addEventListener("click", () => {
             card.classList.remove("card-opened");
             overlay.classList.remove("active");
-            feed.innerText = "";
+            result.innerText = "";
             feedback.value = "";
         });
     }
